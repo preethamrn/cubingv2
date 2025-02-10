88700ec490efe29c240beda478cd3c56975db387
preethamrn <preetham.in.1997@gmail.com>
2025-02-09 21:59:11 -0800

    Add FMCLive page

---
+++ pages/FMCLive.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- TODO: add cube display -->
+    <!-- TODO: integrate bluetooth connection-->
+    <!-- TODO: bluetooth move will transform the cube display -->
+    <!-- TODO: verify rotations also work -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
